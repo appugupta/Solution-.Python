13097ee4bddfe17d65eb22d654ea993f21b7e865
appugupta <91661911+appugupta@users.noreply.github.com>
2022-04-15 15:25:23 +0530

    Add files via upload

---
+++ division.py
@@ -0,0 +1,10 @@
+# Python: Division - Hacker Rank Solution
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    # Python: Division - Hacker Rank Solution START 
+    print(a//b);
+    print(a/b);
+    # Python: Division - Hacker Rank Solution END 
