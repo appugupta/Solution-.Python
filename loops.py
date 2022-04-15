560b586e5a814847e7294930bad04e47c8f79e6f
appugupta <91661911+appugupta@users.noreply.github.com>
2022-04-15 15:27:22 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,7 @@
+# Loops in Python - Hacker Rank Solution
+if __name__ == '__main__':
+    n = int(raw_input())
+    # Loops in Python - Hacker Rank Solution START
+    for i in range(n):
+        print(i*i);
+    # Loops in Python - Hacker Rank Solution END
