5047735b66d2749f18bfaef59c8be7eafd0d1051
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-05-25 21:11:47 +0530

    Create list_into_set.py

---
+++ list_into_set.py
@@ -0,0 +1,8 @@
+# Creating a list of ten data elements  
+the_names = ["George", "Josh", "James", "Mark", "Carlo", "James", "Andy", "Sara", "Andy", "Victor"]  
+  
+# Converting the above list to the set  
+the_unique_names = set( the_names)  
+  
+# Printing the set to the users  
+print( the_unique_names)
