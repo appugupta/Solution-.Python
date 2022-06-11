52aea47559f05d5feea88b0df2e4657e30a317d8
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-06-11 22:55:10 +0530

    Update amstrong_number.py

--- amstrong_number.py
+++ amstrong_number.py
@@ -7,10 +7,8 @@ def power_1(A, B):
             
     return A * power(A, B // 2) * power(A, B // 2)  
     
-# Function for calculating "order of the number"  
 def order_1(A):  
     
-    # Variable for storing the number  
     N = 0  
     while (A != 0):  
         N = N + 1  
@@ -18,7 +16,6 @@ def order_1(A):
             
     return N  
     
-# Function for checking if the given number is Armstrong number or not  
 def is_Armstrong(A):  
         
     N = order_1(A)  
@@ -30,9 +27,7 @@ def is_Armstrong(A):
         sum_1 = sum_1 + power_1(R_1, N)  
         temp_1 = temp_1 // 10  
     
-    # If the above condition is satisfied, it will return the result  
     return (sum_1 == A)  
     
-# Driver code  
 A = int(input("Please enter the number to be checked: "))  
 print(is_Armstrong(A))
